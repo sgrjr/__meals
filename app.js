b5f0e9c9fa5d533d74bd348cbc340ab37233de95
Stephen Reynolds <sgrjr@deliverance.me>
2016-04-20 11:04:00 -0400

    hhh

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // mongoose config
-require('./database');
+//require('./database');
 
 var express = require('express');
 var path = require('path');
@@ -23,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.use('/graphql', graphql);
 app.use('/', routes);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
